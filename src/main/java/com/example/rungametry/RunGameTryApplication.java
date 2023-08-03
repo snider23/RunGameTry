e02beadec22a6165d9e5b9f90250bbe460f7c815
szabo_andras <andreas.szabo23@gmail.com>
2023-08-03 20:38:18 +0200

    Initial commit

---
+++ src/main/java/com/example/rungametry/RunGameTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.rungametry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunGameTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunGameTryApplication.class, args);
+	}
+
+}
